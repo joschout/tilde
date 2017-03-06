c80ee1c31baefa4d4682bda16f963e7e0c93c588
joschout <jonas.schouterden@student.kuleuven.be>
2017-03-06 20:16:35 +0100

    refactored refinement one literal into mutiple methods for term generation. Started on refinement based on conjunctions.

--- representation/language.py
+++ representation/language.py
@@ -266,33 +266,20 @@ class TypeModeLanguage(BaseLanguage):
         # If there is no recently added body literal,
         #       we will have to start from scratch, from an empty set
         if query.get_literal() is not None:
-            generated = query.get_literal().refine_state
+            already_generated_literals = query.get_literal().refine_state
         else:
-            generated = set()
+            already_generated_literals = set()
 
         # We need to generate a literal to refine the body of the clause
         #
         # 1) a positive refinement
-        # We have defined which literals can be added in MODES,
+        # We have defined which literals can be added in _refinement_modes,
         # as their functor and the mode of each of the arguments.
         # We will consider each functor as a possible candidate for refinement
         #
         # SO for each functor in MODES
         for functor, argument_mode_indicators in self._refinement_modes:  # e.g. 'parent', ['+', '+']
-            # We have defined which literals can be added in MODES,
-            # as their functor and the mode of each of the arguments.
-            # We will consider each functor as a possible candidate for refinement
-
-            # we will collect the possible arguments for the current functor in a list
-            arguments = self.get_possible_arguments_for(functor, argument_mode_indicators, variables_in_query_by_type)
-
-            # arguments is a list of lists.
-            # It contains as many lists as the arity of the functor.
-            # Each list corresponds to the possible values in the refined literal of the corresponding variable.
-            # To generate all possible combinations,
-            # we take the carthesian product of the elements in the lists.
-            #
-            # SO for each possible combination of arguments:
+            # for each possible combination of arguments:
             #   create a term using the functor and the arguments.
             #   IF the term hasn't already been generated in the past:
             #       IF we want to break symmetries
@@ -308,25 +295,33 @@ class TypeModeLanguage(BaseLanguage):
             #   save a shallow copy of the set of generated literals
             # ELSE
             #   save the UNION of the seg of generated literals and {t, -t, t_i, -t_i}
-            for args in product(*arguments):
-                t = Term(functor, *args)
-                if t not in generated:
+            # for args in product(*arguments):
+            #     t = Term(functor, *args)
+
+            possible_term_generator = self.__get_possible_terms_for(functor, argument_mode_indicators, variables_in_query_by_type)
+
+            for t in possible_term_generator:
+                if t not in already_generated_literals:
                     if self._symmetry_breaking:
-                        generated.add(t)
+                        already_generated_literals.add(t)
                     t_i = t.apply(TypeModeLanguage.ReplaceNew(nb_of_vars_in_query))
                     t_i.prototype = t
                     if self._symmetry_breaking:
-                        t_i.refine_state = generated.copy()
+                        t_i.refine_state = already_generated_literals.copy()
                     else:
-                        t_i.refine_state = generated | {t, -t, t_i, -t_i}
+                        t_i.refine_state = already_generated_literals | {t, -t, t_i, -t_i}
                     yield t_i
 
-    def get_possible_arguments_for(self, functor, argument_mode_indicators, variables_in_query_by_type):
+    def __get_possible_arguments_for(self, functor, argument_mode_indicators, variables_in_query_by_type):
+        # INPUT e.g. 'parent', ['+', '+'], [A}
+
         # we will collect the possible arguments for the current functor in a list
         arguments = []
         arity = len(argument_mode_indicators)
         # get the types of the arguments of the given predicate
         argument_types = self.get_argument_types(functor, arity)  # type: TypeArguments
+        # e.g parent/2 has argument types ['person', 'person']
+        # argument types are necessary for unification
 
         # a functor has multiple variables,
         #       each with their own modes.
@@ -361,81 +356,18 @@ class TypeModeLanguage(BaseLanguage):
                 raise ValueError("Unknown mode specifier '%s'" % argmode)
         return arguments
 
-    # TODO: verwijder dit
-    refinement_bags = []
-
-    def refine_conjunction_one_literal_experimental(self, query: TILDEQuery) -> Iterator[Term]:
-        # 1. we need to know how many variables are in the already existing rule.
-        #    We also need to know the types of these variables.
-        #    This is important information when adding new literals.
-        nb_of_vars_in_query = len(query.get_variables())
-        variables_in_query_by_type = self.get_variable_types(*query.get_literals())  # type: Dict[TypeName, List[Term]]
-
-        # We check the most recently added body literal.
-        # If there is a most recently added body literal,
-        #   then we check whether its functor is '_recursive'
-        #   If its most recently added body literal is '_recursive',
-        #       we cannot extend the body any further,
-        #       so the function returns.
-        #   Else, we get the set of already generated literals from the last added literal
-        # If there is no recently added body literal,
-        #       we will have to start from scratch, from an empty set
-        if query.get_literal() is not None:
-            generated = query.get_literal().refine_state
-        else:
-            generated = set()
-
-        # We need to generate a literal to refine the body of the clause
+    def __get_possible_terms_for(self, functor, argument_mode_indicators, variables_in_query_by_type):
+        arguments = self.__get_possible_arguments_for(functor, argument_mode_indicators, variables_in_query_by_type)
+        # arguments is a list of lists.
+        # It contains as many lists as the arity of the functor.
+        # Each list corresponds to the possible values in the refined literal of the corresponding variable.
+        # To generate all possible combinations,
+        # we take the carthesian product of the elements in the lists.
         #
-        # 1) a positive refinement
-        # We have defined which literals can be added in _refinement_modes,
-        # as their functor and the mode of each of the arguments.
-        # We will consider each functor as a possible candidate for refinement
-        #
-        # SO for each functor in MODES
-        for refinement_bag in self.refinement_bags:  # e.g. 'parent', ['+', '+']
-            # We have defined which literals can be added in MODES,
-            # as their functor and the mode of each of the arguments.
-            # We will consider each functor as a possible candidate for refinement
-
-            # we will collect the possible arguments for the current functor in a list
-            possible_arguments_of_literal_to_add = self.get_possible_arguments_for(functor, argument_mode_indicators, variables_in_query_by_type)
-
-            # arguments is a list of lists.
-            # It contains as many lists as the arity of the functor.
-            # Each list corresponds to the possible values in the refined literal of the corresponding variable.
-            # To generate all possible combinations,
-            # we take the carthesian product of the elements in the lists.
-            #
-            # SO for each possible combination of arguments:
-            #   create a term using the functor and the arguments.
-            #   IF the term hasn't already been generated in the past:
-            #       IF we want to break symmetries
-            #            add the term to the set of already generated terms
-            #       Substitute each of the '#'-vars for a new variable
-            #       add the term t as the prototype of the substuted term
-            # We know want to be return the substituted term
-            # But before returning,
-            # we want to save the state of this function,
-            # so we can generate new terms next time we call this function.
-            # To do this, we add the set of generated terms to the new literal.
-            # IF we want to break symmetry,
-            #   save a shallow copy of the set of generated literals
-            # ELSE
-            #   save the UNION of the seg of generated literals and {t, -t, t_i, -t_i}
-            for args in product(*possible_arguments_of_literal_to_add):
-                t = Term(functor, *args)
-                if t not in generated:
-                    if self._symmetry_breaking:
-                        generated.add(t)
-                    t_i = t.apply(TypeModeLanguage.ReplaceNew(nb_of_vars_in_query))
-                    t_i.prototype = t
-                    if self._symmetry_breaking:
-                        t_i.refine_state = generated.copy()
-                    else:
-                        t_i.refine_state = generated | {t, -t, t_i, -t_i}
-                    yield t_i
-
+        # SO for each possible combination of arguments:
+        #   create a term using the functor and the arguments.
+        for args in product(*arguments):
+            yield Term(functor, *args)
 
     def get_type_values(self, typename: TypeName) -> ValueSet:
         """Get all values that occur in the data for a given type.
--- representation/refinement_bag.py
+++ representation/refinement_bag.py
@@ -1,5 +1,9 @@
+from typing import Dict
 from typing import List
 from typing import Optional
+
+from representation.TILDE_query import TILDEQuery
+from representation.language import TypeModeLanguage
 
 
 class RefinementBag:
@@ -12,8 +16,28 @@ class RefinementConjunction:
     def __init__(self):
         self.literal_list = []  # type: List[RefinementLiteral]
 
-    def get_possible_conjunctions(self, variables_already_in_query):
-        pass
+    def get_addable_conjunctions(self, variables_already_in_query):
+
+        # a conjunction = 1+ literals
+        # a literal = 1+ variables
+        # we need all possible combinations of moded vars
+
+        # dictionary: variable name to modes
+        variables_in_conjunction = {}  # type: Dict[str, RefinementVar]
+        for literal in self.literal_list:
+            for var in literal.refinement_var_list:
+                if var.name not in variables_in_conjunction:
+                    variables_already_in_query[var.name] = var
+        var_mode_combos = []
+
+        for var in variables_in_conjunction:
+            for mode_of_var in variables_in_conjunction[var].modes:
+                var_mode_combos.append((var, mode_of_var))
+
+
+ 
+        # TODO: types for unification
+        # TODO: unification with variables in query
 
 
 class RefinementLiteral:
@@ -23,6 +47,13 @@ class RefinementLiteral:
 
 
 class RefinementVar:
-    def __init__(self, name: str, modes: List[str]):
+    def __init__(self, name: str, modes: List[str], type: str):
         self.name = name  # type: str
         self.modes = modes  # type: List[str]
+        self.type = type
+
+
+
+def refine_conjunctions(query: TILDEQuery, language: TypeModeLanguage, refinement_bags: List[RefinementBag]):
+    for refinement_bag in refinement_bags:
+        possible_conjunction_generator = refinement_bag.conjunction.get_addable_conjunctions()
