845a9319f4b4f621e3c72492558ea2b47fbb6e1b
joschout <jonas.schouterden@student.kuleuven.be>
2017-07-21 21:08:26 +0200

    Merge branch 'master' into origin/prob_tilde_test
    Moved main into top directory

--- main.py
+++ main.py
@@ -1,51 +1,139 @@
-from problog.logic import Term
+import argparse
+import os
+import sys
+from typing import Optional
 
-from IO.label_collector import LabelCollector
-from IO.parsing_background_knowledge import parse_background_knowledge
-from IO.parsing_examples import parse_examples_key_format_with_key
-from IO.parsing_settings import Settings, SettingParser, KeysPredictionGoalHandler
-from IO.utils import KnowledgeBaseFormat
-from classification.classification_helper import get_example_databases
-from representation.language import TypeModeLanguage
-from trees.TreeBuilder import TreeBuilder, DeterministicTreeBuilder
-from trees.pruning import prune_leaf_nodes_with_same_label
-from trees.stop_criterion import StopCriterionMinimalCoverage
-from trees.tree_converter import KeyTreeToProgramConverter, DeterministicTreeToProgramConverter
+from IO.utils import KnowledgeBaseFormat, InternalExampleFormat
+from run.run_keys import run_keys_simpleprogram, run_keys_clausedb
+from run.run_models import run_models_clausedb, run_models_simpleprogram
+
+kb_suffix = '.kb'
+bg_suffix = '.bg'
+s_suffix = '.s'
 
 
-def run_keys(file_name_labeled_examples, file_name_settings, file_name_background, use_SimpleProgram=False):
+class ProgramSettings:
+    def __init__(self):
+        self.debug_parsing = False  # type: bool
+        self.kb_format = None  # type: Optional[KnowledgeBaseFormat]
+        self.internal_examples_format = InternalExampleFormat.CLAUSEDB  # type: InternalExampleFormat
+        self.filename_prefix = ""  # type: str
+        self.use_bg = False  # type: bool
+        self.use_mle = False  # type: bool
 
-    setting_parser = SettingParser.get_key_settings_parser()  # type: SettingParser
-    setting_parser.parse(file_name_settings)
-    settings = setting_parser.settings  # type: Settings
-    prediction_goal_handler = settings.get_prediction_goal_handler()  # type: KeysPredictionGoalHandler
-    prediction_goal = prediction_goal_handler.get_prediction_goal()  # type: Term
+    @staticmethod
+    def make_program_settings(arguments) -> 'ProgramSettings':
+        settings = ProgramSettings()
 
-    language = settings.language  # type: TypeModeLanguage
+        # get the name of the logic program
+        fname_prefix = arguments.file
+        # check if this file actually exists
+        possible_kb_fname = fname_prefix + kb_suffix
+        possible_s_fname = fname_prefix + s_suffix
 
-    background_knw = parse_background_knowledge(file_name_background)
+        if not os.path.isfile(possible_kb_fname):
+            raise FileNotFoundError("Could not find knowledge base file: " + possible_kb_fname)
+        if not os.path.isfile(possible_s_fname):
+            raise FileNotFoundError("Could not find settings file: " + possible_s_fname)
+        # we now know there exist a kb and s file
+        settings.filename_prefix = fname_prefix
+        # is there a bg file?
+        possible_bg_fname = fname_prefix + bg_suffix
+        settings.use_bg = os.path.isfile(possible_bg_fname)
 
-    examples = parse_examples_key_format_with_key(file_name_labeled_examples)
+        if arguments.debug:
+            settings.debug_parsing = True
 
-    example_dbs = get_example_databases(examples, background_knw)
+        input_format = arguments.format
+        print(input_format)
+        if input_format == 'k' or input_format == 'key':
+            settings.kb_format = KnowledgeBaseFormat.KEYS
+        elif input_format == 'm' or input_format == 'models':
+            settings.kb_format = KnowledgeBaseFormat.MODELS
+        else:
+            raise Exception("required argument for --format has to be k/key or m/models")
 
-    # === getting the labels ===
-    index_of_label_var = prediction_goal_handler.get_predicate_goal_index_of_label_var()  # type: int
-    label_collector = LabelCollector(prediction_goal, index_of_label_var, background_knw)
-    label_collector.extract_labels_dbs(example_dbs)
-    possible_labels = label_collector.get_labels()
-    # =================================
+        if arguments.ex:
+            internal_examples_format = arguments.ex
+            if internal_examples_format == 'c' or internal_examples_format == 'clausedb':
+                settings.internal_examples_format = InternalExampleFormat.CLAUSEDB
+            elif internal_examples_format == 's' or internal_examples_format == 'simpleprogram':
+                settings.internal_examples_format = InternalExampleFormat.SIMPLEPROGRAM
+            else:
+                raise Exception('Got unknown input for argument internal example format: ' + str(arguments.ex))
 
-    tree_builder = DeterministicTreeBuilder(language, background_knw, possible_labels, StopCriterionMinimalCoverage(4))
-    tree_builder.debug_printing(True)
-    tree_builder.build_tree(example_dbs, prediction_goal)
+        if arguments.mle:
+            settings.use_mle = True
 
-    tree = tree_builder.get_tree()
-    print(tree.to_string())
+        return settings
 
-    prune_leaf_nodes_with_same_label(tree)
-    print(tree.to_string())
 
-    treeToProgramConverter = DeterministicTreeToProgramConverter(KnowledgeBaseFormat.KEYS, prediction_goal, index_of_label_var, debug_printing=True)
-    program = treeToProgramConverter.convert_tree_to_simple_program(tree, language)
-    print(program)
+def make_argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()  # type: argparse.ArgumentParser
+    # parser.parse_args('-v', '--verbose', help='helps increase verbosity',
+    #                   action='store_true')
+
+    parser.add_argument('file', help=("Filename prefix to the first order logic files."
+                                      "Both the knowledge base ('*.kb') and settings ('*.s') files "
+                                      "have to have the same name."))
+
+    parser.add_argument('format', help=('Used to explicitly specify the format of the input knowledge base.'
+                                        ' This can be key/k or models/m'),
+                        choices=['m', 'models', 'k', 'key'])
+
+    parser.add_argument('-e', '--ex', help=('Used to explicitly specify how the examples have to be stored internally.'
+                                            'This can be clausedb or simpleprogram. Normally, clausedb is faster.'),
+                        choices=['c', 'clausedb', 's', 'simpleprogram'])
+
+    parser.add_argument('-d', '--debug', help='Use debug mode',
+                        action='store_true')
+
+    parser.add_argument('--mle', help='Use MLE if possible',
+                        action='store_true')
+    return parser
+
+
+def run_program(settings: ProgramSettings):
+    # get the name of the program to run
+    fname_labeled_examples = settings.filename_prefix + kb_suffix
+    fname_settings = settings.filename_prefix + s_suffix
+    if settings.use_bg:
+        fname_background_knowledge = settings.filename_prefix + bg_suffix
+    else:
+        fname_background_knowledge = None
+    debug_printing = settings.debug_parsing
+    use_mle = settings.use_mle
+
+    if settings.kb_format is None:
+        raise NotImplementedError('Automatic recognition of input format is not yet supported.')
+    elif settings.kb_format is KnowledgeBaseFormat.MODELS:
+        if settings.internal_examples_format is InternalExampleFormat.CLAUSEDB:
+            run_models_clausedb(fname_labeled_examples, fname_settings, fname_background_knowledge, debug_printing,
+                                use_mle)
+        elif settings.internal_examples_format is InternalExampleFormat.SIMPLEPROGRAM:
+            run_models_simpleprogram(fname_labeled_examples, fname_settings, fname_background_knowledge, debug_printing,
+                                     use_mle)
+        else:
+            raise NotImplementedError("Only the internal formats SimpleProgram and ClauseDB are supported.")
+    elif settings.kb_format is KnowledgeBaseFormat.KEYS:
+        if settings.internal_examples_format is InternalExampleFormat.CLAUSEDB:
+            run_keys_clausedb(fname_labeled_examples, fname_settings, fname_background_knowledge, debug_printing,
+                              use_mle)
+        elif settings.internal_examples_format is InternalExampleFormat.SIMPLEPROGRAM:
+            run_keys_simpleprogram(fname_labeled_examples, fname_settings, fname_background_knowledge, debug_printing,
+                                   use_mle)
+        else:
+            raise NotImplementedError("Only the internal formats SimpleProgram and ClauseDB are supported.")
+    else:
+        raise NotImplementedError('Only the input formats Models and Key are supported.')
+
+
+def main(argv=sys.argv[1:]):
+    argparser = make_argument_parser()  # type: argparse.ArgumentParser
+    arguments = argparser.parse_args(argv)
+    program_settings = ProgramSettings.make_program_settings(arguments)
+    run_program(program_settings)
+
+
+if __name__ == '__main__':
+    main()
