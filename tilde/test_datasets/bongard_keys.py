03d8125b9624e7a1b8ebfc0ad8d8c4fcf9170e62
Jonas Schouterden <joschout@merlin.cs.kuleuven.be>
2017-10-23 12:42:57 +0200

    test run time bongard

--- tilde/test_datasets/bongard_keys.py
+++ tilde/test_datasets/bongard_keys.py
@@ -1,19 +1,24 @@
+import statistics
+import timeit
+
+import gc
+
 from tilde.IO.parsing_settings.setting_parser import KeysSettingsParser
 from tilde.representation.example import InternalExampleFormat
 from tilde.run.run_keys import run_keys
 from tilde.trees.TreeBuilder import TreeBuilderType
 
-file_name_labeled_examples = 'D:\\KUL\\KUL MAI\\Masterproef\\data\\ACE-examples-data\\ace\\bongard\\keys\\bongard.kb'
-file_name_settings = 'D:\\KUL\\KUL MAI\\Masterproef\\data\\ACE-examples-data\\ace\\bongard\\keys\\bongard.s'
+file_name_labeled_examples = '/home/joschout/Documents/tilde_data/ACE-examples-data/ace/bongard/keys/bongard.kb'
+file_name_settings = '/home/joschout/Documents/tilde_data/ACE-examples-data/ace/bongard/keys/bongard.s'
 
 use_clausedb = True
 
 debug_printing_example_parsing = False
-debug_printing_tree_building = True
-debug_printing_tree_pruning = True
+debug_printing_tree_building = False
+debug_printing_tree_pruning = False
 debug_printing_program_conversion = True
-debug_printing_get_classifier = True
-debug_printing_classification = True
+debug_printing_get_classifier = False
+debug_printing_classification = False
 
 parsed_settings = KeysSettingsParser().parse(file_name_settings)
 
@@ -24,6 +29,13 @@ if use_clausedb:
 else:
     internal_ex_format = InternalExampleFormat.SIMPLEPROGRAM
 
+
+# times = []
+#
+# for i in range(0, 10):
+#
+start = timeit.default_timer()
+
 run_keys(file_name_labeled_examples, parsed_settings, internal_ex_format, treebuilder_type,
          debug_printing_example_parsing=debug_printing_example_parsing,
          debug_printing_tree_building=debug_printing_tree_building,
@@ -32,3 +44,10 @@ run_keys(file_name_labeled_examples, parsed_settings, internal_ex_format, treebu
          debug_printing_get_classifier=debug_printing_get_classifier,
          debug_printing_classification=debug_printing_classification
          )
+#     gc.collect()
+end = timeit.default_timer()
+print("time", end - start)
+
+# times.append(end-start)
+#
+# print("average duration:", statistics.mean(times), "seconds")
