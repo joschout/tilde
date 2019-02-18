a92e26480040aefd1e1b2e1234adfa4f42094163
joschout <jonasschouterden@gmail.com>
2019-02-18 15:44:18 +0100

    back_end_picking's imports are now wrapped together with choosing the backend

--- refactor/back_end_picking.py
+++ refactor/back_end_picking.py
@@ -1,11 +1,5 @@
 from enum import Enum
 
-import refactor
-import refactor.query_testing_back_end.problog.defaults
-import refactor.query_testing_back_end.django.defaults
-import refactor.query_testing_back_end.flgg_py4j.defaults
-import refactor.query_testing_back_end.subtle.defaults
-
 from refactor.default_interface import DefaultHandler
 
 
@@ -19,18 +13,39 @@ class QueryBackEnd(Enum):
     FLGG = 6
 
 
+class UnavailableBackEndException(Exception):
+    pass
+
+
 def get_back_end_default(description) -> DefaultHandler:
     if description == QueryBackEnd.SIMPLE_PROGRAM:
-        return refactor.query_testing_back_end.problog.defaults.ProblogDefaultHandler(QueryBackEnd.SIMPLE_PROGRAM.name)
+        try:
+            import refactor.query_testing_back_end.problog.defaults
+            return refactor.query_testing_back_end.problog.defaults.ProblogDefaultHandler(
+                QueryBackEnd.SIMPLE_PROGRAM.name)
+        except ImportError as err:
+            raise UnavailableBackEndException(description.name + " backend not available, " + str(err))
     if description == QueryBackEnd.PROBLOG or \
             description == QueryBackEnd.CLAUSEDB:
         return None
     if description == QueryBackEnd.DJANGO:
-        return refactor.query_testing_back_end.django.defaults.DjangoDefaultHandler(QueryBackEnd.DJANGO.name)
+        try:
+            import refactor.query_testing_back_end.django.defaults
+            return refactor.query_testing_back_end.django.defaults.DjangoDefaultHandler(QueryBackEnd.DJANGO.name)
+        except ImportError as err:
+            raise UnavailableBackEndException(description.name + " backend not available, " + str(err))
     if description == QueryBackEnd.FLGG:
-        return refactor.query_testing_back_end.flgg_py4j.defaults.FLGGDefaultHandler(QueryBackEnd.FLGG.name)
+        try:
+            import refactor.query_testing_back_end.flgg_py4j.defaults
+            return refactor.query_testing_back_end.flgg_py4j.defaults.FLGGDefaultHandler(QueryBackEnd.FLGG.name)
+        except ImportError as err:
+            raise UnavailableBackEndException(description.name + " backend not available, " + str(err))
     if description == QueryBackEnd.SUBTLE:
-        return refactor.query_testing_back_end.subtle.defaults.SubtleDefaultHandler(QueryBackEnd.SUBTLE.name)
+        try:
+            import refactor.query_testing_back_end.subtle.defaults
+            return refactor.query_testing_back_end.subtle.defaults.SubtleDefaultHandler(QueryBackEnd.SUBTLE.name)
+        except ImportError as err:
+            raise UnavailableBackEndException(description.name + " backend not available, " + str(err))
     else:
         return None
 
